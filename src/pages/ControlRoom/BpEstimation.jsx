79d45c5974dc3d6729853a2d80692738f3934815
M. Syauqi Fadhlika <ukisyauqi@gmail.com>
2023-10-20 11:23:05 +0700

    add account auth

--- src/pages/ControlRoom/BpEstimation.jsx
+++ src/pages/ControlRoom/BpEstimation.jsx
@@ -91,32 +91,6 @@ export default function BpEstimation() {
   const [baruLogin, setBaruLogin] = useState(true)
 
   // helper functions
-  function R(min) {
-    return Math.round(Math.random() * 10 + min);
-  }
-  function RR(min, range) {
-    return Math.round(Math.random() * range + min);
-  }
-  function getSystole() {
-    let a = age / 10;
-    a = Math.round(a)
-    if (systole != "" && typeof systole === 'number') return R(systole - 5);
-    if (a == 0) return R(90);
-    if (a == 1) return R(100);
-    if (a == 2) return R(110);
-    if (a == 3) return R(120);
-    return R(130);
-  }
-  function getDiastole() {
-    let a = age / 10;
-    a = Math.round(a)
-    if (diastole != "" && typeof diastole === 'number') return R(diastole - 5);
-    if (a == 0) return R(50);
-    if (a == 1) return R(60);
-    if (a == 2) return R(70);
-    if (a == 3) return R(80);
-    return R(90);
-  }
   function getCurrentDateTime() {
     const currentDate = new Date();
 
